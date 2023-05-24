2bbeea12f37ec6724926ae5a94a0834fbf0a4056
tuana <tuananh45468@gmail.com>
2023-05-24 18:30:38 +0700

    modal client component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,6 +12,7 @@ export default function DashboardLayout({ children }) {
           <Sidebar></Sidebar>
           {children}
         </GlassPane>
+        <div id={"modal"}></div>
       </body>
     </html>
   );
