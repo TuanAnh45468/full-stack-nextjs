789befe72b97abf16a98bcc2e17e6896993c3928
tuana <tuananh45468@gmail.com>
2023-05-24 09:41:54 +0700

    greetings component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,15 @@
 import Head from "../head";
 import GlassPane from "@/components/GlassPane";
 import "@/styles/global.css";
+import Sidebar from "@/components/Sidebar";
 
 export default function DashboardLayout({ children }) {
   return (
     <html lang="en">
       <Head />
       <body className="h-screen w-screen candy-mesh p-6">
-        <GlassPane className="w-full h-full flex items-center justify-center">
+        <GlassPane className="w-full h-full flex items-center">
+          <Sidebar></Sidebar>
           {children}
         </GlassPane>
       </body>
